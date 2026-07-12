0c77a21779d22f01bf856b951b991d6c0d68b6fe
Mauricio <mauric75@gmail.com>
2026-07-12 02:32:51 -0300

    PWA: instalar como app en el celular

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'mandragora-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/escuela.html',
+  '/sala.html',
+  '/compania.html',
+  '/cine-viajero.html',
+  '/agenda.html',
+  '/comunidad.html',
+  '/contacto.html',
+  '/reservar.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
